942094a035eb13266593a95ae419e02e849e2913
monaallam62 <monaallam5102@gmail.com>
2026-02-25 16:36:50 +0200

    Methods

--- Assignment_oop1/Person.cs
+++ Assignment_oop1/Person.cs
@@ -38,62 +38,61 @@ namespace Assignment_oop1
     //    }
     //}
     #endregion
-
     #region Question 5
-    public struct SeatL
-    {
-        public char Row;
-        public int Number;
+    //public struct SeatL
+    //{
+    //    public char Row;
+    //    public int Number;
 
-        public SeatL(char _row, int _number)
-        {
-            Row = _row;
-            Number = _number;
-        }
-        public override string ToString()
-        {
-            return $"{Row}{Number}";
-        }
-    }
-    public class Ticket
-    {
-        public string MovieName;
-        public TicketType Type;
-        public SeatL Seat;
-        private double Price;
+    //    public SeatL(char _row, int _number)
+    //    {
+    //        Row = _row;
+    //        Number = _number;
+    //    }
+    //    public override string ToString()
+    //    {
+    //        return $"{Row}{Number}";
+    //    }
+    //}
+    //public class Ticket
+    //{
+    //    public string MovieName;
+    //    public TicketType Type;
+    //    public SeatL Seat;
+    //    private double Price;
 
-        public Ticket(string _MovieName, TicketType _type, SeatL _seat, double _price)
-        {
-            MovieName = _MovieName;
-            Type = _type;
-            Seat = _seat;
-            Price = _price;
-        }
-        public Ticket(string _MovieName) : this(_MovieName, TicketType.Standard, new SeatL('A', 1), 50)
-        {
+    //    public Ticket(string _MovieName, TicketType _type, SeatL _seat, double _price)
+    //    {
+    //        MovieName = _MovieName;
+    //        Type = _type;
+    //        Seat = _seat;
+    //        Price = _price;
+    //    }
+    //    public Ticket(string _MovieName) : this(_MovieName, TicketType.Standard, new SeatL('A', 1), 50)
+    //    {
             
-        }
-        public double CalcTotal(double taxPercent)
-        {
-            return Price + (Price * taxPercent / 100);
-        }
-        public void ApplyDiscount(ref double discountAmount)
-        {
-            if(discountAmount>0 && discountAmount<= Price)
-            {
-                Price -= discountAmount;
-                discountAmount = 0;
-            }
-        }
-        public void PrintTicket(double taxPercent)
-        {
-            Console.WriteLine($"Movie : {MovieName}");
-            Console.WriteLine($"Type : {Type}");
-            Console.WriteLine($"Seat : {Seat}");
-            Console.WriteLine($"Price : {Price}");
-            Console.WriteLine($"Total ({taxPercent}% tax) : {CalcTotal(taxPercent)}");
+    //    }
+    //    public double CalcTotal(double taxPercent)
+    //    {
+    //        return Price + (Price * taxPercent / 100);
+    //    }
+    //    public void ApplyDiscount(ref double discountAmount)
+    //    {
+    //        if(discountAmount>0 && discountAmount<= Price)
+    //        {
+    //            Price -= discountAmount;
+    //            discountAmount = 0;
+    //        }
+    //    }
+    //    public void PrintTicket(double taxPercent)
+    //    {
+    //        Console.WriteLine($"Movie : {MovieName}");
+    //        Console.WriteLine($"Type : {Type}");
+    //        Console.WriteLine($"Seat : {Seat}");
+    //        Console.WriteLine($"Price : {Price}");
+    //        Console.WriteLine($"Total ({taxPercent}% tax) : {CalcTotal(taxPercent)}");
 
-        }
-    } 
+    //    }
+    //} 
     #endregion
 }
